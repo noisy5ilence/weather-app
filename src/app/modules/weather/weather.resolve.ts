f2f1b600ed988fd22533ec0f3c63b7a87608c9fc
Ner <inerono@gmail.com>
2018-06-20 14:28:02 +0300

    Catch wrong city id.

--- src/app/modules/weather/weather.resolve.ts
+++ src/app/modules/weather/weather.resolve.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
 import { WeatherService, Forcast } from './weather.service';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs/internal/observable/of'
 
 
 @Injectable()
 export class WeatherResolve implements Resolve<Forcast> {
-  constructor(private _data: WeatherService) {}
+  constructor(
+    private _data: WeatherService,
+    private _router: Router
+  ) {}
 
-  resolve(route: ActivatedRouteSnapshot) {
-    return this._data.getWeatherForecastFor5Days(route.params.id);
+  resolve(route: ActivatedRouteSnapshot): Observable<Forcast> {
+    return this._data.getWeatherForecastFor5Days(route.params.id).pipe(
+      catchError((error) => {
+        this._router.navigate(['/']);
+        return of(error);
+      })
+    );
   }
 }
